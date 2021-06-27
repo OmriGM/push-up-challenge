bacd3a4e0c1519005817027114933a91cb00c162
OmriGM <omrigr123@gmail.com>
2021-06-28 01:07:14 +0300

    Init project with CRA and lean optimizations

---
+++ README.md
@@ -0,0 +1,3 @@
+# Push Up Challenge
+
+Online fun push-up game with AI counter!
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+    return (
+        <div className={'app'}>
+            <p>Welcome to the smart AI push up challenge</p>
+        </div>
+    )
+};
+
+export default App;
